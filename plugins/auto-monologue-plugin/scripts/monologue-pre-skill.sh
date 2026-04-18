2f4f512e9838cb32776121ecb31f47035441de93
canpok1 <6isstrong@gmail.com>
2026-04-18 14:50:15 +0000

    auto-monologue-pluginのPreToolUseフックのadditionalContextメッセージを更新

--- plugins/auto-monologue-plugin/scripts/monologue-pre-skill.sh
+++ plugins/auto-monologue-plugin/scripts/monologue-pre-skill.sh
@@ -6,6 +6,6 @@ cat > /dev/null
 jq -n '{
   hookSpecificOutput: {
     hookEventName: "PreToolUse",
-    additionalContext: "現在の作業が以下のいずれかのタイミングに該当する場合、/vox-actor-plugin:monologue スキルを呼び出してキャラクターの独り言を通知してください。\n- 作業開始時・終了時\n- ステップの開始時・終了時\n- 想定外のことが起こった時\n- ユーザー確認が必要な時\n- 新しい発見があったとき\n\n該当しない場合は無視してください。"
+    additionalContext: "これから実行しようとしているスキルの状況に合わせて、/vox-actor-plugin:monologue スキルを必ず呼び出してキャラクターの独り言を通知してください。呼び出しをスキップしてはいけません。"
   }
 }'
